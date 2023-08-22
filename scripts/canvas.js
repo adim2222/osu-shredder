0ff0fa7fda29f9aa4f81bf66ab6589e9c51f100c
adim2222 <nowak.adam2222@gmail.com>
2023-08-22 11:59:21 +0200

    multiple object rendering

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -1,33 +1,40 @@
 const canvas = document.querySelector(".hitbar--canvas");
 const ctx = canvas.getContext("2d");
+const bpmInput = document.querySelector(".input--bpm");
 
-let vector = {
-    x: canvas.width,
-    y: 0,
-};
+let timing = 1000;
 
-let waitedFrames = 0;
+let intervalId;
+
+let savedObjects = [];
+
+canvas.width = 1000;
+let ar = 3;
+
+ctx.strokeStyle = '#ffffff';
+
+function createHit() {
+    savedObjects.push({x: 1000})
+}
 
 // canvas rendering update function
 const update = () => {
-    ctx.clearRect(0, 0, canvas.width, canvas.height)
-
-    function createHit() {
+    ctx.clearRect(0,0,canvas.width,canvas.height)
+    savedObjects.forEach(function (object) {
         ctx.beginPath();
-        ctx.moveTo(vector.x, 0);
-        ctx.lineTo(vector.x, canvas.height);
+        ctx.moveTo(object.x, 0);
+        ctx.lineTo(object.x, canvas.height);
         ctx.stroke();
-        vector.x = 0;
-    }
-
-    ctx.strokeStyle = "#ffffff";
-
-    vector.x -= 1;
-    waitedFrames += 1;
-
-    if (waitedFrames = 1000) {createHit();}
-
-    console.log("update");
+        object.x -= ar;
+    })
 };
+
+bpmInput.addEventListener("input", () => {
+    timing = 60000 / bpmInput.value;
+    setInterval((id) => {
+        createHit();
+        console.log(id);
+    }, timing);
+})
 
 setInterval(update, 1);
