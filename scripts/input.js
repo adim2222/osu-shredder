fc2c073871f1a4169680c4f7cefa5f1391ca4935
adim2222 <121557893+adim2222@users.noreply.github.com>
2024-03-01 14:37:43 +0100

    added localstorage keysaving

--- scripts/input.js
+++ scripts/input.js
@@ -1,17 +1,19 @@
 const key1input = document.querySelector(".key--input__1");
 const key2input = document.querySelector(".key--input__2");
 
-let key1 = "z";
-let key2 = "x";
+let key1 = localStorage.getItem("key1") || "z";
+let key2 = localStorage.getItem("key2") || "x";
 
 const getKey1 = () => {
     key1 = key1input.value.toLowerCase();
     document.querySelector(".hitbar--key1").innerHTML = key1.toUpperCase();
+    localStorage.setItem("key1", `${document.querySelector(".hitbar--key1").innerHTML.toUpperCase()}`);
 };
 
 const getKey2 = () => {
     key2 = key2input.value.toLowerCase();
     document.querySelector(".hitbar--key2").innerHTML = key2.toUpperCase();
+    localStorage.setItem("key2", `${document.querySelector(".hitbar--key2").innerHTML.toUpperCase()}`);
 };
 
 key1input.addEventListener("input", getKey1);
